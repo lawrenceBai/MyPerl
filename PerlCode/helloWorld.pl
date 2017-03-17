fa7149d119a79035b396639093e3d377aca1e275
lawrenceBai <tioit@sina.cn>
2017-03-17 16:55:54 +0800

    my perl codes

---
+++ PerlCode/helloWorld.pl
@@ -0,0 +1,15 @@
+
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+#----------------
+print "my first perl\n";
+
+
+
+
+
+
+
---
+++ PerlCode/testPerl.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+#
+#######################
+use warnings;
+use strict;
+
+my @str=(1..100);
+
+print @str;
+
+
+
